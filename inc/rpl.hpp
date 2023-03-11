9d8cf3b6f74c868a9e2f559a3d24ad638104c13a
Tomas alejandro Martinez cruz <tomartin@c2r1s1.42madrid.com>
2023-03-11 18:48:32 +0100

    pre fusion

--- inc/rpl.hpp
+++ inc/rpl.hpp
@@ -6,7 +6,7 @@
 /*   By: tomartin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/03 11:45:18 by tomartin          #+#    #+#             */
-/*   Updated: 2023/03/11 17:17:28 by tomartin         ###   ########.fr       */
+/*   Updated: 2023/03/11 18:47:32 by tomartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,7 @@
 # define RPL_ENDOFMOTD() (":End of MOTD command\r\n")
 # define RPL_YOUREOPER() (":You are now an IRC operator\r\n")
 # define RPL_REHASHING(config_file) (config_file + " :Rehashing\r\n")
-# define RPL_TIME(server, time_serveur) (server + " :" + time_serveur + "\r\n")
+# define RPL_TIME(server, time_server) (server + " :" + time_server + "\r\n")
 # define RPL_USERSSTART() (":UserID Terminal HosHost\r\n")
 # define RPL_USERS(users_rpy) (":" + users_rpy + "\r\n")
 # define RPL_ENDOFUSERS() (":End of users\r\n")
